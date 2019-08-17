b4c158c9e986d5fdee732eefe9d6e6bf21c09ccb
dmheisel <d.m.heisel@gmail.com>
2019-08-16 23:07:59 -0500

    database PUT route completed to update likes

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,35 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require('../modules/pool');
 const galleryItems = require('../modules/gallery.data');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+	console.log(req.params);
+	const galleryId = req.params.id;
+	const sqlQuery = `UPDATE "photos"
+            SET "likes" = ("likes" + 1)
+            WHERE "id" = $1;`;
+
+	pool
+		.query(sqlQuery, [galleryId])
+		.then(result => {
+			console.log('successful PUT route to database, updated photo likes');
+			res.sendStatus(201);
+		})
+		.catch(err => {
+			console.log(`error on PUT route to database: ${err}`);
+			res.sendStatus(500);
+		});
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    const sqlQuery = `SELECT * FROM "photos";`
-    pool
-        .query(sqlQuery)
-        .then(result => {
-            console.log('successful GET route to database')
-            res.send(result.rows);
-        })
-        .catch(err => {
-            console.log(`error on GET route to database: ${err}`)
-            res.sendStatus(500)
-        })
+    const sqlQuery =
+        `SELECT * FROM "photos"
+        ORDER BY "id";`;
+	pool
+		.query(sqlQuery)
+		.then(result => {
+			console.log('successful GET route to database');
+			res.send(result.rows);
+		})
+		.catch(err => {
+			console.log(`error on GET route to database: ${err}`);
+			res.sendStatus(500);
+		});
 }); // END GET Route
 
 module.exports = router;
