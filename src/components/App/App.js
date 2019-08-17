dd16e1f7be20cd1a9c0697e78edc9862b01682f9
dmheisel <d.m.heisel@gmail.com>
2019-08-17 12:19:51 -0500

    clientside and serverside post handshake completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // import './App.css';
 import Axios from 'axios';
-import GalleryList from '../GalleryList/GalleryList'
-import Container from '@material-ui/core/Container'
+import GalleryList from '../GalleryList/GalleryList';
+import Container from '@material-ui/core/Container';
 // import CssBaseline from '@material-ui/core/CssBaseline'
 
 class App extends Component {
@@ -12,45 +12,57 @@ class App extends Component {
 	};
 	getImages = () => {
 		//gets images from the server using axios
-		Axios
-			.get('/gallery')
+		Axios.get('/gallery')
 			.then(response => {
 				console.log('successful GET request from server');
-        this.setState({ galleryList: response.data });
-        console.log(this.state)
+				this.setState({ galleryList: response.data });
+				console.log(this.state);
 			})
 			.catch(error => {
 				console.log('error on GET request from server: ', error);
 			});
-  }
+	};
 
-  addLike = (id) => {
-    //PUT method to add to love count of photo on server using axios
-    Axios
-      .put(`/gallery/like/${id}`)
-      .then(response => {
-        console.log(`successful PUT request to server: ${response}`)
-        this.getImages()
-      })
-      .catch(error => {
-        console.log(`error on PUT request to server: ${error}`)
-      })
-  }
-  componentDidMount = () => {
-    console.log('app loaded');
-    this.getImages()
-  }
+	addLike = id => {
+		//PUT method to add to love count of photo on server using axios
+		Axios.put(`/gallery/like/${id}`)
+			.then(response => {
+				console.log(`successful PUT request to server: ${response}`);
+				this.getImages();
+			})
+			.catch(error => {
+				console.log(`error on PUT request to server: ${error}`);
+			});
+	};
 
+	postPicture = picture => {
+		Axios.post('/gallery')
+			.then(response => {
+				console.log(`successful POST route to server: ${response}`);
+				this.getImages();
+			})
+			.catch(error => {
+				console.log(`error on POST route to server: ${error}`);
+			});
+	};
+
+	componentDidMount = () => {
+		console.log('app loaded');
+		this.getImages();
+	};
 
 	render() {
 		return (
-			<Container maxWidth="lg" className='App'>
+			<Container maxWidth='lg' className='App'>
 				{/* <CssBaseline /> */}
 				{/* <header className='App-header'>
 					<h1 className='App-title'>Gallery of my life</h1>
 				</header>
 				<br /> */}
-        <GalleryList galleryList={this.state.galleryList} addLike={this.addLike}/>
+				<GalleryList
+					galleryList={this.state.galleryList}
+					addLike={this.addLike}
+				/>
 			</Container>
 		);
 	}
