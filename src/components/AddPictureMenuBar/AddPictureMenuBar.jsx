8bf76fd55d835f79ea7e7e2a2e761254549e6f3e
dmheisel <d.m.heisel@gmail.com>
2019-08-17 22:09:27 -0500

    removed unneeded import

--- src/components/AddPictureMenuBar/AddPictureMenuBar.jsx
+++ src/components/AddPictureMenuBar/AddPictureMenuBar.jsx
@@ -6,7 +6,6 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MenuBarInputs from '../MenuBarInputs/MenuBarInputs'
-import { Z_FIXED } from 'zlib';
 
 const styles = theme => ({
   root: {
